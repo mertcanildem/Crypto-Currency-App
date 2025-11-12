6c9aac1735aaaa823f5f17b3d66536864a803c8a
mertildem <mertcanildem9@gmail.com>
2025-11-12 11:00:05 +0300

    Setting up Assets folder and Custom Color Theme #1

---
+++ CryptoApp/Extensions/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by Mert can Ildem on 12.11.2025.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
